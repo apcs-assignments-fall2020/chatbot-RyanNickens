794ccac538b8904b3263cf9528e7498c362f77b5
RyanNickens <22rnickens@athenian.org>
2020-09-02 19:29:23 +0000

    made code ask 5 questions

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -4,6 +4,27 @@ public class MyMain {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
 
+        System.out.print("What is your name? ");
+        String name = scan.next();
+        System.out.println("Hello, " + name + "! It's Nice to meet you!");
+
+        System.out.print(name + ", what is your favorite color?");
+        String color = scan.next();
+        System.out.println("I quite enjoy " + color + " too!");
+
+        System.out.print("What grade are you in?");
+        String grade = scan.next();
+        System.out.println("Grade " + grade + " is a tough one!");
+
+        System.out.print("What is your favorite animal " + name + "?");
+        String animal = scan.next();
+        System.out.println("The " + animal + " is one majestic animal!");
+
+        System.out.print("What is your least favorite food?");
+        String food = scan.next();
+        System.out.println("Yuck! " + food + " does not sound tasty!");
+
+
 
         scan.close();
     }
